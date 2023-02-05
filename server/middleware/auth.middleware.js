0cae5d5d8532c09e8f2bf79d83b7ea99056d50d9
Дмитрий Маклаков <dimonabinsk@yandex.ru>
2023-02-05 23:06:34 +0300

    Update auth.middleware.js

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -5,7 +5,6 @@ module.exports = (req, res, next) => {
     return next();
   }
   try {
-    // Bearer sytduufhjgchkglh
     const token = req.headers.authorization.split(" ")[1];
     if (!token) {
       return res.status(401).json({ message: "Unauthorized" });
